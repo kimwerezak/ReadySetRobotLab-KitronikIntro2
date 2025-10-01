7fd111a6d4a29411605197175c90354de45bdb02
Kimberley Werezak <122053493+kimwerezak@users.noreply.github.com>
2025-10-01 16:15:38 -0600

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,42 +1,25 @@
-#Zip LED's together to have all 4 lights change at the same time.
-#You can alter the the rgb values to change the color
+#The Robot will move forward until something is within 10cm of its distance sensor.
+#If something is in the way, it will stop, LEDS will change and resume moving in 3 seconds.
+
+#Zip LED's together to have all 4 lights change at the same time
 All_LED = Kitronik_Move_Motor.create_move_motor_zipled(4)
-All_LED.set_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.rgb(255, 0, 255)))
-All_LED.show()
-basic.pause(1000)
 
-#To control each light seperately, we've created a variable name (ex. LED_0 for the LED labeled 0)
-#for each LED. Range will indicate which the Led you wish to control and how many subsequent
-#lights respectively.  This allows for grouping as well. Set to the color of your choice and show.
-
-#Changes LED 0 to purple.
-LED_0 = All_LED.range(0, 1)
-LED_0.set_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.PURPLE))
-LED_0.show()
-
-#Changes LED 0 to red.
-LED_1 = All_LED.range(1, 1)
-LED_1.set_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.RED))
-LED_1.show()
-
-#Changes LED 0 to blue.
-LED_2 = All_LED.range(2, 1)
-LED_2.set_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.BLUE))
-LED_2.show()
-
-#Changes LED 0 to green.
-LED_3 = All_LED.range(3, 1)
-LED_3.set_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.GREEN))
-LED_3.show()
-
-#The Robot will move forward until something is within 15cm of its distance sensor.
-#If something is in the way, it will stop, honk and resume moving in 3 seconds.
 def Do_not_cut_me_off():
-    if Kitronik_Move_Motor.measure() < 15:
-        Kitronik_Move_Motor.stop()
-        Kitronik_Move_Motor.beep_horn()
-        basic.pause(3000)
-    else: Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 20)
+    Kitronik_Move_Motor.stop()
+    
+    #You can alter the the rgb values to change the color
+    All_LED.set_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.RED))
+    All_LED.show()
+        
+    basic.pause(3000)
+        
 
         
-basic.forever(Do_not_cut_me_off)
+def on_forever2():
+    while Kitronik_Move_Motor.measure() > 15:
+        All_LED.set_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.GREEN))
+        All_LED.show()
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 10)
+
+    Do_not_cut_me_off()
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,37 +1,20 @@
-// Zip LED's together to have all 4 lights change at the same time.
-// You can alter the the rgb values to change the color
+// The Robot will move forward until something is within 10cm of its distance sensor.
+// If something is in the way, it will stop, LEDS will change and resume moving in 3 seconds.
+// Zip LED's together to have all 4 lights change at the same time
 let All_LED = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
-All_LED.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.rgb(255, 0, 255)))
-All_LED.show()
-basic.pause(1000)
-// To control each light seperately, we've created a variable name (ex. LED_0 for the LED labeled 0)
-// for each LED. Range will indicate which the Led you wish to control and how many subsequent
-// lights respectively.  This allows for grouping as well. Set to the color of your choice and show.
-// Changes LED 0 to purple.
-let LED_0 = All_LED.range(0, 1)
-LED_0.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Purple))
-LED_0.show()
-// Changes LED 0 to red.
-let LED_1 = All_LED.range(1, 1)
-LED_1.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
-LED_1.show()
-// Changes LED 0 to blue.
-let LED_2 = All_LED.range(2, 1)
-LED_2.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
-LED_2.show()
-// Changes LED 0 to green.
-let LED_3 = All_LED.range(3, 1)
-LED_3.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Green))
-LED_3.show()
-// The Robot will move forward until something is within 15cm of its distance sensor.
-// If something is in the way, it will stop, honk and resume moving in 3 seconds.
-basic.forever(function Do_not_cut_me_off() {
-    if (Kitronik_Move_Motor.measure() < 15) {
-        Kitronik_Move_Motor.stop()
-        Kitronik_Move_Motor.beepHorn()
-        basic.pause(3000)
-    } else {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 20)
+function Do_not_cut_me_off() {
+    Kitronik_Move_Motor.stop()
+    // You can alter the the rgb values to change the color
+    All_LED.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
+    All_LED.show()
+    basic.pause(3000)
+}
+
+basic.forever(function on_forever2() {
+    while (Kitronik_Move_Motor.measure() > 15) {
+        All_LED.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Green))
+        All_LED.show()
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 10)
     }
-    
+    Do_not_cut_me_off()
 })
